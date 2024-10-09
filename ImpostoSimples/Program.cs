b10ad6ff461a08a3059333ef26a419f8b9a7a0b6
hilton.lssantos <hilton.lssantos@RIP0576973W10-1>
2024-10-09 17:10:36 -0300

    Adicionar arquivos de projeto.

---
+++ ImpostoSimples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpostoSimples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal salario, imposto;
+
+            Console.WriteLine("Informe o valor do seu salário: ");
+            salario = decimal.Parse(Console.ReadLine());
+
+            if (salario >= 3000)
+            {
+                imposto = salario * 10 / 100;
+                salario = salario + imposto;
+                Console.WriteLine("Salario com 10% de imposto: " + salario.ToString("C"));
+            }
+            else if (salario <= 2999)
+            {
+                Console.WriteLine("Voce é isento! Salario" +salario.ToString("C"));
+            }
+            Console.ReadKey();
+        }
+    }
+}
